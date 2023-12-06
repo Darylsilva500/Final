4abecfbef376ce2ceedf44d6ede607a4f0b296ca
Darylsilva500 <151389566+Darylsilva500@users.noreply.github.com>
2023-12-06 22:51:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ with st.container():
     st.write(
         "Welcome to Huawei, a global leader in technology and innovation, connecting people and societies across the world. With a commitment to building a fully connected, intelligent world, Huawei is at the forefront of transforming industries, empowering individuals, and driving positive change."
     )
-     st.markdown("[To Learn More About My Project](https://github.com/Darylsilva500/Final/tree/main)")
+     st.markdown("[To Learn More About My Project](https://github.com/Darylsilva500/Final/tree/main")
 
 # ---- SIDEBAR MENU ----
 with st.sidebar:
