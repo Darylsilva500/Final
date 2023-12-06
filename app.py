669b2c580fe9cfdb91dde76e88a6c5e191d38fb8
Darylsilva500 <151389566+Darylsilva500@users.noreply.github.com>
2023-12-06 23:44:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ with st.container():
      
 # ---- SIDEBAR MENU ----
 with st.sidebar:
-    st.image(profile_user, use_column_width=True, output_format="PNG", channels="RGB", format="PNG")
+    st.image(profile_user, use_column_width=True")
     st.markdown(
         """
         <style>
