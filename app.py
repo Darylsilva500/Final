673959245618a1c04cfeac48804540d5f80d8b94
Darylsilva500 <151389566+Darylsilva500@users.noreply.github.com>
2023-12-07 02:03:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,18 @@ img_background = Image.open("images/background.png")
 profile_user = Image.open("images/user.png")
 
 # ---- HEADER SECTION ----
-with st.container():
-image_log = "images/log.png"
+with st.beta_container():
+    image_log = "images/log.png"
 
-col1, col2 = st.beta_columns([1, 3])
+    col1, col2 = st.beta_columns([1, 3])
 
-with col1:
-    st.image(image_log, width=100)
+    with col1:
+        st.image(image_log, width=100)
 
-with col2:
-    st.subheader("Hi, I'm :red[Daryl D. Silva] :satellite_antenna:", unsafe_allow_html=True)
-    st.title("Huawei: Connecting the World through Innovation")
+    with col2:
+        st.subheader("Hi, I'm :red[Daryl D. Silva] :satellite_antenna:", unsafe_allow_html=True)
+        st.title("Huawei: Connecting the World through Innovation")
+
     st.write(
         "Welcome to Huawei, a global leader in technology and innovation, connecting people and societies across the world. With a commitment to building a fully connected, intelligent world, Huawei is at the forefront of transforming industries, empowering individuals, and driving positive change."
     )
